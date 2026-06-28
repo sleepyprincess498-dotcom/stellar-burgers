a90bd706b5133084872c60a3b8116a08bc19c1a7
Nikakos <sleepyprincess498@gmail.com>
2026-06-28 19:45:49 +0300

    fixed redirect from login and register

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,23 +1,12 @@
-import { FC, SyntheticEvent, useEffect, useState } from 'react';
+import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from '../../services/store';
+import { useDispatch } from '../../services/store';
 import { loginUser } from '../../services/middlewares/loginUser';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const isAuth = useSelector((state) => state.auth.isAuth);
-  const location = useLocation();
-  const from = location.state?.from?.pathname || '/profile';
-
-  useEffect(() => {
-    if (isAuth) {
-      navigate(from, { replace: true });
-    }
-  }, [isAuth]);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,24 +1,13 @@
-import { FC, SyntheticEvent, useEffect, useState } from 'react';
+import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
-import { useDispatch, useSelector } from '../../services/store';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from '../../services/store';
 import { registerUser } from '../../services/middlewares/registerUser';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const isAuth = useSelector((state) => state.auth.isAuth);
-  const location = useLocation();
-  const from = location.state?.from?.pathname || '/profile';
-
-  useEffect(() => {
-    if (isAuth) {
-      navigate(from, { replace: true });
-    }
-  }, [isAuth]);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
